3989f6952bdbd1a6193204a8e785ca83d09ac2cc
orange492 <40350423+orange492@users.noreply.github.com>
2022-11-16 22:19:52 +0900

    이미지추가

--- Minigame_Tower/Assets/Scripts/Defence/Manager/DefenceManager.cs
+++ Minigame_Tower/Assets/Scripts/Defence/Manager/DefenceManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
+using System;
 
 public class DefenceManager : Singleton<DefenceManager>
 {
