4c363548a3c09557d27b569c42555da9d5042a90
skl509 <77062634+skl509@users.noreply.github.com>
2022-11-24 23:40:25 +0900

    수정

---
+++ Minigame_Tower/Assets/Scripts/Puzzle/Score/Fever.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Fever : MonoBehaviour
+{
+     Slider slider;
+    int feverCount=0;
+    BlockController blockController;
+    bool isFevering = false;
+
+    public bool IsFevering
+    {
+        get => isFevering;
+        set
+        {
+            isFevering = value;
+            if (IsFevering)
+            {
+                blockController.OnFever();
+            }
+            else
+            {
+                blockController.OffFever();
+            }
+        }
+    }
+
+
+    private void Awake()
+    {
+        slider = transform.GetChild(0).GetComponent<Slider>();
+    }
+    private void Start()
+    {
+        blockController = FindObjectOfType<BlockController>();
+        blockController.onFeverChange += FeverChange;
+
+    }
+
+
+    private void Update()
+    {
+       
+        if (isFevering)
+        {
+
+            slider.value -= Time.deltaTime * 200.0f;
+            if (slider.value <= 0)
+            {
+                IsFevering = false;
+            }
+        }
+    }
+    private void FeverChange(int fever)
+    {
+        if (!isFevering) 
+        {
+            feverCount += fever;
+            if (feverCount > 1000)
+            {
+                feverCount = 1000;
+            }
+            slider.value = feverCount;
+            
+
+            if (feverCount >= 1000)
+            {
+                IsFevering = true;
+                feverCount = 0;
+            }
+        }
+    }
+
+    
+
+
+}
